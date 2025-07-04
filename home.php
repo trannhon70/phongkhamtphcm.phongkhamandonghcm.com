2b0bedb49a24942b663933d3619df8dba6d292a0
trannhon70 <kevintran351996@gmail.com>
2025-07-04 09:48:24 +0700

    update

---
+++ home.php
@@ -0,0 +1,174 @@
+<?php include "inc/header.php" ?>
+<link rel="stylesheet" href="<?php echo $local ?>/css/trang-chu.min.css">
+</head>
+
+<body>
+    <?php include_once 'layout/headerLayout.php' ?>
+     <main>
+        <article>
+            <?php include "layout/sliderLayout.php" ?>
+            
+             <div class="checkSendPhoneMobile"  style="margin-top: 170px;" ></div>
+            <section id="menu_logo_mobile" >
+                <?php include 'layout/menu_logo.php' ?> 
+            </section>
+            
+
+          <section class="motto" id="motto">
+                <div class="motto__container">
+                    <div class="motto__container-left">
+                        <div class="motto__container-left-top">
+                            <div class="motto__container-left-top-img">
+                                <amp-img class="header__bottom-logo-img" src="<?php echo $local ?>/images/icons/icon_pen.webp" height="60px" width="50px" alt="..."></amp-img>
+                            </div>
+                            <div class="motto__container-left-top-title">
+                                PHƯƠNG CHÂM CỦA CHÚNG TÔI
+                            </div>
+                        </div>
+                        <div class="motto__container-left-boddy" >
+                        <div class="motto__container-left-text">
+                          
+                            Phòng khám Chuyên khoa An Đông là cơ sở y tế uy tín tại TP.HCM. Cung cấp dịch vụ y tế chất lượng và tận tâm. Phòng khám với nhiều chuyên khoa, với đội ngũ y bác sĩ giàu kinh nghiệm, luôn sẵn sàng chăm sóc và hỗ trợ điều trị cho người bệnh.
+                        </div>
+                        </div>
+                    </div>
+                    <div class="motto__container-right">
+                        <img class="motto__container-right-img" src="<?php echo $local ?>/images/users/phuong_cham.webp" alt="...">
+                        <img width="100%" class="motto__container-right-img-mobile"  src="<?php echo $local ?>/images/users/khong_gian1.webp" alt="...">
+                    </div>
+                </div>
+            </section>
+
+             <section class="advantage" id="advantage">
+                <div class="advantage__container">
+                    <div class="advantage__container-left">
+                        <img class="advantage__container-left-img" src="<?php echo $local ?>/images/users/u_the.webp" alt="...">
+                    </div>
+                    <div class="advantage__container-right">
+                        <div class="advantage__container-right-top">
+                            <div class="advantage__container-right-top-img">
+                                <amp-img class="header__bottom-logo-img" src="<?php echo $local ?>/images/icons/icon_check.webp" height="60px" width="60px" alt="..."></amp-img>
+                            </div>
+                            <div class="advantage__container-right-top-title">
+                                ƯU THẾ VƯỢT TRỘI
+                            </div>
+                        </div>
+                        <div class="advantage__container-right-bottom">
+                            <div class="advantage__container-right-bottom-item">
+                                <amp-img class="header__bottom-logo-img" src="<?php echo $local ?>/images/icons/icon_check1.webp" height="25px" width="25px" alt="..."></amp-img>
+                                <h5>Đội ngũ bác sĩ chuyên môn cao</h5>
+                            </div>
+                            <div class="advantage__container-right-bottom-item">
+                                <amp-img class="header__bottom-logo-img" src="<?php echo $local ?>/images/icons/icon_check1.webp" height="25px" width="25px" alt="..."></amp-img>
+                                <h5>môi trường thân thiện, dịch vụ tận tâm, chu đáo</h5>
+                            </div>
+                            <div class="advantage__container-right-bottom-item">
+                                <amp-img class="header__bottom-logo-img" src="<?php echo $local ?>/images/icons/icon_check1.webp" height="25px" width="25px" alt="..."></amp-img>
+                                <h5>hệ thống máy móc hiện đại</h5>
+                            </div>
+                            <div class="advantage__container-right-bottom-item">
+                                <amp-img class="header__bottom-logo-img" src="<?php echo $local ?>/images/icons/icon_check1.webp" height="25px" width="25px" alt="..."></amp-img>
+                                <h5>chi phí khám hỗ trợ điều trị bệnh hợp lý</h5>
+                            </div>
+                            <div class="advantage__container-right-bottom-item">
+                                <amp-img class="header__bottom-logo-img" src="<?php echo $local ?>/images/icons/icon_check1.webp" height="25px" width="25px" alt="..."></amp-img>
+                                <h5>thủ tục nhanh gọn, không rườm rà</h5>
+                            </div>
+                            <div class="advantage__container-right-bottom-item">
+                                <amp-img class="header__bottom-logo-img" src="<?php echo $local ?>/images/icons/icon_check1.webp" height="25px" width="25px" alt="..."></amp-img>
+                                <h5>môi trường thân thiện</h5>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+           <?php include_once 'layout/benefitLayout.php' ?>
+
+            <section class="space" id="space">
+                <div class="space__title">
+                    <div>KHÔNG GIAN PHÒNG KHÁM</div>
+                </div>
+                <div class="space__container">
+                    <div class="space__container-left">
+                        <img src="<?php echo $local ?>/images/users/khong_gian1.webp" alt="...">
+                    </div>
+
+                    <div class="space__container-right">
+                        <div class="space__container-right-img space__container-right-img-mobile">
+                            <img src="<?php echo $local ?>/images/users/khong_gian2.webp" alt="...">
+                        </div>
+                        <div class="space__container-right-img">
+                            <img src="<?php echo $local ?>/images/users/khong_gian3.webp" alt="...">
+                        </div>
+                        <div class="space__container-right-img">
+                            <img src="<?php echo $local ?>/images/users/khong_gian4.webp" alt="...">
+                        </div>
+                        <div class="space__container-right-img">
+                            <img src="<?php echo $local ?>/images/users/khong_gian5.webp" alt="...">
+                        </div>
+                        <div class="space__container-right-img">
+                            <img src="<?php echo $local ?>/images/users/khong_gian6.webp" alt="...">
+                        </div>
+                        <div class="space__container-right-img space__container-right-img-mobile">
+                            <img src="<?php echo $local ?>/images/users/khong_gian7.webp" alt="...">
+                        </div>
+                        <div class="space__container-right-img space__container-right-img-mobile">
+                            <img src="<?php echo $local ?>/images/users/khong_gian8.webp" alt="...">
+                        </div>
+                        <div class="space__container-right-img space__container-right-img-mobile">
+                            <img src="<?php echo $local ?>/images/users/khong_gian9.webp" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+             <section class="client" id="client">
+                <div class="client__container">
+                    <div class="client__container-title">KHÁCH HÀNG NÓI GÌ?</div>
+                    <div class="client__container-list">
+                        <div class="client__container-item ">
+                            <amp-img class="client__container-item-img" src="<?php echo $local ?>/images/users/user_kimOanh.webp" height="100px" width="100px" alt="..."></amp-img>
+                            <div class="client__container-item-name">CHỊ KIM OANH</div>
+                            <div class="client__container-item-div">
+                                <hr>
+                            </div>
+                            <div class="client__container-item-text">
+                                 “Tôi rất hài lòng với dịch vụ tại phòng khám. Các bác sĩ rất tận tình, giải thích rõ ràng về bệnh tình và phương pháp điều trị. Không gian khám bệnh sạch sẽ, thoải mái, giúp tôi cảm thấy yên tâm hơn.”
+                            </div>
+                        </div>
+                        <div class="client__container-item activeClient">
+                            <amp-img class="client__container-item-img" src="<?php echo $local ?>/images/users/user_trunghieu.webp" height="100px" width="100px" alt="..."></amp-img>
+                            <div class="client__container-item-name">ANH TRUNG HIẾU</div>
+                            <div class="client__container-item-div">
+                                <hr>
+                            </div>
+                            <div class="client__container-item-text">
+                                  “Tuần trước mình có đến đây cắt bao quy đầu. Các bác sĩ tư vấn rất nhiệt tình, có tâm, tay nghề giỏi, làm rất êm và kỹ lưỡng, mình thấy giá cũng hợp lý. Bên đây cũng có khám sức khỏe định kỳ nữa nên tháng sau mình sẽ đưa gia đình đến khám.”
+                            </div>
+                        </div>
+                        <div class="client__container-item ">
+                            <amp-img class="client__container-item-img" src="<?php echo $local ?>/images/users/user_minhHai.webp" height="100px" width="100px" alt="..."></amp-img>
+                            <div class="client__container-item-name">ANH ĐỨC TÀI</div>
+                            <div class="client__container-item-div">
+                                <hr>
+                            </div>
+                            <div class="client__container-item-text">
+                                “Mình được bạn bè giới thiệu đến Chuyên khoa An Đông. Tại đây, các bác sĩ vô cùng có tâm với nghề, nên mình cũng thoải mái chia sẻ những vấn đề mình đang gặp phải. Sau điều trị, mình cảm thấy sức khỏe cải thiện hơn”
+                            </div>
+                        </div>
+                    </div>
+                    <div class="index-client">
+                        <div class="client-item client-item-0 activeClient"></div>
+                        <div class="client-item client-item-1"></div>
+                        <div class="client-item client-item-2"></div>
+                    </div>
+                </div>
+            </section>
+
+
+        </article>
+    </main>
+    
+     <?php include_once 'inc/footer.php' ?>
